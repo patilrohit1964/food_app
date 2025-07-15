4411dcec769b63359a4ef0fd75a8e65800382815
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-15 16:07:16 +0530

    done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,25 @@
-import Navbar from "@/components/header/Navbar";
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <Navbar />
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-1">
+        <section className="relative h-[80vh] max-h-[800px]">
+          <div className="absolute inset-0 bg-black/50 z-10">
+            <Image
+              src={"/res.jpeg"}
+              alt="hero image"
+              fill
+              className="object-cover"
+            />
+            <div className="container relative z-20 h-full flex flex-col justify-center items-start text-white mx-4">
+              <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                Authentic flavour, <br /> <span>Exceptional</span> Dinning
+              </h1>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
