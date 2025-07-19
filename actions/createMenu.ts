fd9bd3b144eb354bf5dc392d7868bc0719c41771
patilrohit1964 <patilrohit19649@gmail.com>
2025-07-19 14:39:49 +0530

    add bc

--- actions/createMenu.ts
+++ actions/createMenu.ts
@@ -1,4 +1,7 @@
 "use server";
+import { prisma } from "@/lib/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 type CreateMenuFormState = {
   errors: {
@@ -27,6 +30,38 @@ export const createMenuAction = async (initialState, formData: FormData) => {
     description: formData.get("description") as string,
     category: formData.get("category") as string,
     price: formData.get("price") as string,
-    image: formData.get("imageUrl") as string,
+    image: formData.get("image") as string,
   });
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  try {
+    // save data in db
+    await prisma.menuItem.create({
+      data: {
+        name: result.data.name,
+        description: result.data.description,
+        category: result.data.category,
+        price: Number(result.data.price),
+        imageUrl: result.data.image!,
+      },
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error);
+      return {
+        errors: [error.message],
+      };
+    } else {
+      return {
+        errors: {
+          formError: ["An unexpected error"],
+        },
+      };
+    }
+  }
+  revalidatePath("/admin/menu");
+  redirect("/admin/menu");
 };
